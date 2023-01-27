ef6da97d13f695e45599b0f005791556dfee627a
everyzzz <jquisper2@gmail.com>
2023-01-26 23:24:09 -0500

    "fix"

--- src/App.tsx
+++ src/App.tsx
@@ -13,24 +13,6 @@ function App() {
       <Route path="/" element={<Pokedex />}/>
       <Route path="/favoritos" element={<Favorites/>} />
       </Routes>
-      
-      
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <img src={logo} className="App-logo" alt="logo" />
-  //       <p>
-  //         Edit <code>src/App.tsx</code> and save to reload.
-  //       </p>
-  //       <a
-  //         className="App-link"
-  //         href="https://reactjs.org"
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         Learn React
-  //       </a>
-  //     </header>
-  //   </div>
   );
 }
 
